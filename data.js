c2131c7da9e3d25de5f8a920883c1dd053674fbb
DESKTOP-6QCJPT1\chint <chintan9@aol.in>
2018-05-17 21:35:21 -0500

    test

--- data.js
+++ data.js
@@ -98,5 +98,31 @@ var data = [
         "answer": "#61DAFB"
       }
     ]
+  },
+  {
+    "category": "Feeling Lucky",
+    "questions": [
+      {
+        "points": 100,
+        "question": "Using the proposed ECMAScript module syntax, how do you load the Mortgage.js module from the current directory and make all its members available in an object named mortgage?",
+        "answer": "<code>import * as mortgage from './Mortgage';</code>"
+      },
+      {
+        "points": 200,
+        "question": "What are the colors of the olympic rings?",
+        "answer": "<img src='assets/img/olympic_rings.png'/>"
+      },
+      {
+        "points": 300,
+        "question": "What's the date of the first React commit on GitHub?",
+        "answer": "May 26th, 2013"
+      },
+      {
+        "points": 400,
+        "question": "What's the hex color of the React logo?",
+        "answer": "#61DAFB"
+      }
+    ]
   }
+  
 ];

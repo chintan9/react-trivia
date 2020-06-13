109507041d40aebd02ecde2e63be0015d0607c16
Restyled.io <commits@restyled.io>
2020-06-13 00:45:28 +0000

    Restyled by prettier

--- data.js
+++ data.js
@@ -1,189 +1,188 @@
 var data = [
   {
-    category : "CRICKET",
-    questions : [
+    category: "CRICKET",
+    questions: [
       {
-        points : 100,
-        question : "For a cricketer, the initials “LBW” stands for …",
-        answer : "Leg before wicket"
+        points: 100,
+        question: "For a cricketer, the initials “LBW” stands for …",
+        answer: "Leg before wicket",
       },
       {
-        points : 200,
-        question :
-
-            "The biennial international test cricket series between Australia and England is called",
-        answer : "The ashes"
+        points: 200,
+        question:
+          "The biennial international test cricket series between Australia and England is called",
+        answer: "The ashes",
       },
       {
-        points : 300,
-        question :
-            "Who was the first player to reach 10,000 runs in Test cricket?",
-        answer : "Sunil Gavaskar"
+        points: 300,
+        question:
+          "Who was the first player to reach 10,000 runs in Test cricket?",
+        answer: "Sunil Gavaskar",
       },
       {
-        points : 400,
-        question :
-            "Which national team is nicknamed “The Proteas” or “The Springboks”?",
-        answer : "South Africa"
+        points: 400,
+        question:
+          "Which national team is nicknamed “The Proteas” or “The Springboks”?",
+        answer: "South Africa",
       },
       {
-        points : 500,
-        question : "The highest team total record in test cricket is …",
-        answer : "952"
-      }
-    ]
+        points: 500,
+        question: "The highest team total record in test cricket is …",
+        answer: "952",
+      },
+    ],
   },
   {
-    category : "INDIA",
-    questions : [
+    category: "INDIA",
+    questions: [
       {
-        points : 100,
-        question : "Number of State and Union Territories in India",
-        answer : "29, 7"
+        points: 100,
+        question: "Number of State and Union Territories in India",
+        answer: "29, 7",
       },
       {
-        points : 200,
-        question : "The current President of India is ______ .",
-        answer : "Ram Nath Kovind"
+        points: 200,
+        question: "The current President of India is ______ .",
+        answer: "Ram Nath Kovind",
       },
       {
-        points : 300,
-        question : "Which Indian city is nicknamed the 'City of Pearls'?",
-        answer : "Hyderabad"
+        points: 300,
+        question: "Which Indian city is nicknamed the 'City of Pearls'?",
+        answer: "Hyderabad",
       },
       {
-        points : 400,
-        question : "The Palk Straits separates India and which country?",
-        answer : "Sri Lanka"
+        points: 400,
+        question: "The Palk Straits separates India and which country?",
+        answer: "Sri Lanka",
       },
       {
-        points : 500,
-        question : "Number of Countries Where India Shares Its Border",
-        answer : "8"
-      }
-    ]
+        points: 500,
+        question: "Number of Countries Where India Shares Its Border",
+        answer: "8",
+      },
+    ],
   },
   {
-    category : "US",
-    questions : [
+    category: "US",
+    questions: [
       {
-        points : 100,
-        question :
-            "The Hershey Company, commonly known as Hershey, was founded in which U.S. state?",
-        answer : "Pennsylvania"
+        points: 100,
+        question:
+          "The Hershey Company, commonly known as Hershey, was founded in which U.S. state?",
+        answer: "Pennsylvania",
       },
       {
-        points : 200,
-        question :
-            "Which city served as the capital of the United States from 1785 until 1790?",
-        answer : "New York"
+        points: 200,
+        question:
+          "Which city served as the capital of the United States from 1785 until 1790?",
+        answer: "New York",
       },
       {
-        points : 300,
-        question :
-            "How many US states border the Pacific Ocean? Give names also",
-        answer :
-            "Five states: California, Oregon, Washington, Alaska and Hawaii"
+        points: 300,
+        question:
+          "How many US states border the Pacific Ocean? Give names also",
+        answer:
+          "Five states: California, Oregon, Washington, Alaska and Hawaii",
       },
       {
-        points : 400,
-        question :
-            "What's the only U.S. state whose name ends with three consecutive vowels?",
-        answer : "Hawaii"
+        points: 400,
+        question:
+          "What's the only U.S. state whose name ends with three consecutive vowels?",
+        answer: "Hawaii",
       },
       {
-        points : 500,
-        question : "What is the only U.S. state without a rectangular flag?",
-        answer : "Ohio"
-      }
-    ]
+        points: 500,
+        question: "What is the only U.S. state without a rectangular flag?",
+        answer: "Ohio",
+      },
+    ],
   },
   {
-    category : "WORLD",
-    questions : [
+    category: "WORLD",
+    questions: [
       {
-        points : 100,
-        question : "What is the tallest building in the world?",
-        answer : "The Burj Khalifa"
+        points: 100,
+        question: "What is the tallest building in the world?",
+        answer: "The Burj Khalifa",
       },
       {
-        points : 200,
-        question :
-            "The lowest natural temperature ever directly recorded at ground level was measured on what Continent?",
-        answer : "Antarctica"
+        points: 200,
+        question:
+          "The lowest natural temperature ever directly recorded at ground level was measured on what Continent?",
+        answer: "Antarctica",
       },
       {
-        points : 300,
-        question : "Longest border in the world?",
-        answer : "Australia"
+        points: 300,
+        question: "Longest border in the world?",
+        answer: "Australia",
       },
-      {points : 400, question : "Capital of south korea?", answer : "Seoul"}, {
-        points : 500,
-        question : "What's the oldest continuously inhabited city the world?",
-        answer : "Damascus, Syria"
-      }
-    ]
+      { points: 400, question: "Capital of south korea?", answer: "Seoul" },
+      {
+        points: 500,
+        question: "What's the oldest continuously inhabited city the world?",
+        answer: "Damascus, Syria",
+      },
+    ],
   },
   {
-    category : "RIDDLE",
-    questions : [
+    category: "RIDDLE",
+    questions: [
       {
-        points : 100,
-        question :
-            "James's mom has four children. Their names are April, May, June, and ______?",
-        answer : "James"
+        points: 100,
+        question:
+          "James's mom has four children. Their names are April, May, June, and ______?",
+        answer: "James",
       },
       {
-        points : 200,
-        question : "What has 13 hearts, but no other organs?",
-        answer : "A deck of cards"
+        points: 200,
+        question: "What has 13 hearts, but no other organs?",
+        answer: "A deck of cards",
       },
       {
-        points : 300,
-        question :
-            "No matter how little or how much you use me, you change me every month. What am I?",
-        answer : "A Calendar."
+        points: 300,
+        question:
+          "No matter how little or how much you use me, you change me every month. What am I?",
+        answer: "A Calendar.",
       },
       {
-        points : 400,
-        question :
-            "If eleven plus two equals one, what does nine plus five equal?",
-        answer :
-            "11 o'clock plus 2 hours = 1 o'clock /n 9 o'clock plus 5 hours = 2 o'clock"
+        points: 400,
+        question:
+          "If eleven plus two equals one, what does nine plus five equal?",
+        answer:
+          "11 o'clock plus 2 hours = 1 o'clock /n 9 o'clock plus 5 hours = 2 o'clock",
       },
       {
-        points : 500,
-        question :
-            "Name the number that is three more than one-fifth of one-tenth of one-half of 5,000.",
-        answer : "53"
-      }
-    ]
+        points: 500,
+        question:
+          "Name the number that is three more than one-fifth of one-tenth of one-half of 5,000.",
+        answer: "53",
+      },
+    ],
   },
   {
-    category : "BUSINESS",
-    questions : [
+    category: "BUSINESS",
+    questions: [
       {
-        points : 100,
-        question : "Who was the founder and chairman of Blue origin?",
-        answer : "Jeffrey P. Bezos"
+        points: 100,
+        question: "Who was the founder and chairman of Blue origin?",
+        answer: "Jeffrey P. Bezos",
       },
       {
-        points : 200,
-        question : "Advanced Micro Devices Tag line",
-        answer : "The Future Is Fusion"
+        points: 200,
+        question: "Advanced Micro Devices Tag line",
+        answer: "The Future Is Fusion",
       },
       {
-        points : 300,
-        question : "What was Lamborghini first known for?",
-        answer : "Farm Tractors."
+        points: 300,
+        question: "What was Lamborghini first known for?",
+        answer: "Farm Tractors.",
       },
       {
-        points : 400,
-        question : "What major corporation uses the slogan 'Go Brown'?",
-        answer : "UPS"
+        points: 400,
+        question: "What major corporation uses the slogan 'Go Brown'?",
+        answer: "UPS",
       },
-      {points : 500, question : "Who invented first car?", answer : "Karl Benz"}
-    ]
+      { points: 500, question: "Who invented first car?", answer: "Karl Benz" },
+    ],
   },
-
-]
+];
--- js/audio.js
+++ js/audio.js
@@ -3,14 +3,14 @@ sounds.flip = new Audio("assets/audio/card-flip.mp3");
 sounds.flipBack = new Audio("assets/audio/card-flip-back.mp3");
 // sounds.countdown = new Audio("assets/audio/countdown.mp3");
 
-export let play = sound => {
+export let play = (sound) => {
   if (sounds[sound]) {
     sounds[sound].currentTime = 0;
     sounds[sound].play();
   }
 };
 
-export let stop = sound => {
+export let stop = (sound) => {
   if (sounds[sound]) {
     sounds[sound].pause();
   }
